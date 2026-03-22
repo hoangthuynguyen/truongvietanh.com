e971d5823cde13d79d29bcc9dcadd0b670156c48
Nguyen Hoang <hoangthuynguyen@gmail.com>
2026-03-22 07:22:54 +0700

    Restore Cloudflare Workers deployment config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://truongvietanh.com',
-  output: 'static',
+  output: 'server',
+  adapter: cloudflare({
+    imageService: 'compile',
+  }),
 });
